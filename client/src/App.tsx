60346f66a0fb6b93e8c6f21c51df38eeebfe6536
jsdev305 <jsdev305@gmail.com>
2026-03-16 15:10:43 -0400

    add guest and protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,51 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Incidents from "./pages/Incidents";
 import { Toaster } from "sonner";
+import ProviderWrapper from "./providers/ProviderWrapper";
+import ProtectedRoute from "./guards/ProtectedRoute";
+import GuestRoute from "./guards/GuestRoute";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <>
-      <Toaster richColors />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/incidents" element={<Incidents />} />
-      </Routes>
+      <ProviderWrapper>
+        <Toaster richColors />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <GuestRoute>
+                <Login />
+              </GuestRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <GuestRoute>
+                <Register />
+              </GuestRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/incidents"
+            element={
+              <ProtectedRoute>
+                <Incidents />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </ProviderWrapper>
     </>
   );
 }
