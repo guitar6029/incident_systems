07bb2bdfad53495b41aa913cfe67348230965b68
jsdev305 <jsdev305@gmail.com>
2026-03-16 15:07:58 -0400

    add AuthProvider

---
+++ client/src/providers/AuthProvider.tsx
@@ -0,0 +1,48 @@
+import { checkUser } from "@/api/auth";
+import type { AuthUser } from "@/types/AuthUser";
+import type { ReactNode } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface AuthContextType {
+  user: AuthUser | null;
+  loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<AuthUser | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  async function checkAuth() {
+    try {
+      const user = await checkUser();
+      setUser(user);
+    } catch (error) {
+      console.error(`Error : ${error}`);
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within AuthProvider");
+  }
+  return context;
+}
+
+export default AuthProvider;
