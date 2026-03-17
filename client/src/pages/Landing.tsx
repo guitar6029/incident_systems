f5f7b7b76807fa6de02e7360c7f05a1bce9ce0f2
jsdev305 <jsdev305@gmail.com>
2026-03-17 13:06:21 -0400

    add login and register link to landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Landing() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>Incident Command</h1>
+      <div className="flex flex-row items-center gap-2">
+        <Link to={"/login"}>
+          <button>Login</button>
+        </Link>
+        <Link to={"/register"}>
+          <button>Register</button>
+        </Link>
+      </div>
     </div>
   );
 }
