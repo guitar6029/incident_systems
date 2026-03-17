2291ede38f304349b273b2474c4e961181d178e7
jsdev305 <jsdev305@gmail.com>
2026-03-17 17:52:23 -0400

    added casts and attributes to Incident model

--- backend/app/Models/Incident.php
+++ backend/app/Models/Incident.php
@@ -3,18 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\IncidentStatus;
 
 class Incident extends Model
 {
+    protected $casts = [
+        'status' => IncidentStatus::class
+    ];
+
+    protected $attributes = [
+        'status' => 'open',
+    ];
+
     protected $fillable = [
         'title',
         'description',
-        'severity'
+        'severity',
+        'status'
     ];
 
     public function scopeSeverity($query, $severity)
     {
-        return $query->where("severity", $severity);
+        return $query->where("severity", strtolower($severity));
     }
 
     public function user()
